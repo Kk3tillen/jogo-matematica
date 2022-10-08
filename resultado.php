665cac95247b53cb6e2eef558d66e6515685434d
Kézia Ketillen <k.kezia@academico.ifrn.edu.br>
2022-10-07 22:48:18 -0300

    atualização arquivos

--- resultado.php
+++ resultado.php
@@ -28,20 +28,28 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Matematicando</title>
-    <link rel="stylesheet" href="css/estilo.css">
+    <link rel="stylesheet" href="style/styleResultado.css">
 </head>
 <body>
     <?php if($acertou == true){ ?>
         <div class="resultado">
-            <img src="https://i0.wp.com/abes-dn.org.br/wp-content/uploads/2022/05/quality-g096c0fb43_1920.png?fit=1200%2C1200&ssl=1" alt="deu certo!">
-            <h1>Parabéns, Você acertou!</h1>
-            <a href="/">Tentar Novamente</a>
+            <div class="certa">
+                <img class="fogos" src="img\fogos.svg" alt="deu certo!">
+                <h1>Parabéns<br>
+                Você acertou!!</h1>
+                <img class="fogos" src="img\fogos.svg" alt="deu certo!">
+            </div>
+            <a href="/"><img id="reload" src="img\reload.svg" alt="deu certo!"> Jogar Novamente</a>
         </div>
     <?php } else { ?>  
-        <div class="resultado">
-            <img src="https://imagensemoldes.com.br/wp-content/uploads/2020/08/Imagem-X-Vermelho-PNG-1015x1024.png" alt="deu ruim!">
-            <h1>Que pena, Você errou!</h1>
-            <a href="/">Tentar Novamente</a>
+        <div class="resultado2">
+            <div class="errado">
+                <img class="sad" src="img\sad.svg" alt="triste">
+                <h1>Que pena, Você errou!</h1>
+            </div>
+            <div class="tentarNovamente">
+                <a href="/"><img id="reload" src="img\reload.svg" alt="você errou!">Tentar Novamente</a>
+            </div>
         </div>
     <?php }?> 
 </body>
